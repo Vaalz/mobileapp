ed3fa9a72283712c70ccf4ba3d5e4b0cc6db28dc
Valz <revalfahrizal87@gmail.com>
2025-05-14 14:41:44 +0700

    test

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js}", "./*.html"],
+
+   theme: {
+     extend: {
+      fontFamily: {
+        'pacifico': ['Pacifico', 'cursive']
+      },
+     },
+   },
+   plugins: [],
+ }
